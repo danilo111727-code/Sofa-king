43e5c534b17a415c7254889106e4fe07a17bd393
danilo111727-code <danilo111727@gmail.com>
2026-04-17 17:45:40 -0300

    design: logo um pouco maior no mobile (h-10→h-12)

--- artifacts/sofa-king/src/components/layout/Navbar.tsx
+++ artifacts/sofa-king/src/components/layout/Navbar.tsx
@@ -103,7 +103,7 @@ export function Navbar() {
             <img
               src={logoImg}
               alt="Sofá King"
-              className="h-10 sm:h-14 w-auto object-contain"
+              className="h-12 sm:h-14 w-auto object-contain"
             />
           </Link>
 
